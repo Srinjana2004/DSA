ab236ce17668707721255d9173d9881dbb1ba005
Srinjana Sahana <srinjanasahana@gmail.com>
2026-05-29 10:37:33 +0530

    Add files via upload

---
+++ Leetcode32.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Leetcode32 {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int maxLen = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    maxLen = Math.max(maxLen, i - stack.peek());
+                }
+            }
+        }
+        return maxLen;
+    }
+}
