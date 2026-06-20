fba3da1168a2b55c7061f584e9208cc27b45813c
Srinjana Sahana <srinjanasahana@gmail.com>
2026-06-21 01:51:38 +0530

    Add files via upload

---
+++ Leetcode141.java
@@ -0,0 +1,25 @@
+class Leetcode141 {
+     class ListNode {
+         int val;
+         ListNode next;
+         ListNode(int x) {
+             val = x;
+             next = null;
+         }
+     }
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) return true;
+        }
+
+        return false;
+    }
+}
