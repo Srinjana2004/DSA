e668035968a82fa1f87406d2e5087b2a267771e3
Srinjana Sahana <srinjanasahana@gmail.com>
2026-06-24 19:26:42 +0530

    Add files via upload

---
+++ Leetcode160.java
@@ -0,0 +1,12 @@
+
+public class Leetcode160 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA, b = headB;
+while (a != b) {
+ a = (a == null) ? headB : a.next;
+ b = (b == null) ? headA : b.next;
+}
+return a;
+        
+    }
+}
