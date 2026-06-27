d7d61fa13e88d259a46b1a47911f09f84643acdc
Srinjana Sahana <srinjanasahana@gmail.com>
2026-06-27 11:01:49 +0530

    Add files via upload

---
+++ Leetcode206.java
@@ -0,0 +1,14 @@
+class Leetcode206 {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
