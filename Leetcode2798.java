fd491cbf65a2b55354ac36d772ef149919f9ea51
Srinjana Sahana <srinjanasahana@gmail.com>
2026-09-01 11:21:22 +0530

    Add files via upload

---
+++ Leetcode2798.java
@@ -0,0 +1,12 @@
+public class Leetcode2798 {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int count=0;  
+        for (int h: hours){
+            if(h>=target)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
