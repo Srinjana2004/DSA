d57bee5cd5c5cfd6efa0f81aa83b905e04d1f831
Srinjana Sahana <srinjanasahana@gmail.com>
2026-09-21 13:51:16 +0530

    Add files via upload

---
+++ Sorting.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Sorting {
+    public  static void printArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i]+" ");
+    }
+    System.out.println();
+}
+   public static void main(String[] args) {
+       int arr[]={7,8,3,1,2};
+       for(int i=0;i<arr.length-1;i++){
+        int smallest=i;
+        for(int j=0;j<arr.length-i-1;j++){
+            if(arr[smallest]>arr[j]){
+                // int temp=arr[j];
+                // arr[j]=arr[j+1];
+                // arr[j+1]=temp;
+                smallest=j;
+            }
+        }
+        int temp=arr[smallest];
+        arr[smallest]=arr[i];
+        arr[i]=temp;
+       }
+       printArray(arr);
+   }
+}
